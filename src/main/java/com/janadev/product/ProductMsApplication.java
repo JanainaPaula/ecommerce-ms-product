a66b8ab24fb7d21ac564100b676b7aafcde06514
Janaina Paula <janainadepaula93@gmail.com>
2021-01-25 13:20:09 -0300

    Atualizando o spring version

--- src/main/java/com/janadev/product/ProductMsApplication.java
+++ src/main/java/com/janadev/product/ProductMsApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProductMicroserviceApplication {
+public class ProductMsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProductMicroserviceApplication.class, args);
+		SpringApplication.run(ProductMsApplication.class, args);
 	}
 
 }
